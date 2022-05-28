659e1def63212e44ac712434d7abcfa22b67194d
Lunaisise <lunaisise@hotmail.co.jp>
2022-05-29 00:27:16 +0900

    🚧 複数時刻表同時取得 実装中
    時刻表画面対応

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const {routeName, routeId, stations, trains} = (() => {
         const routeName = sessionStorage.getItem('routeName');
         const routeId = sessionStorage.getItem('routeId');
-        const text = sessionStorage.getItem('trains');
-        if (text === null) {
+        const sessionStorageTrains = sessionStorage.getItem('trains');
+        const sessionStorageStations = sessionStorage.getItem('stations');
+        if ([sessionStorageTrains, sessionStorageStations].includes(null)) {
             return {
                 routeName,
                 routeId,
@@ -11,12 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
                 trains: null
             };
         }
-        const json = JSON.parse(text);
+        const trains = JSON.parse(sessionStorageTrains);
+        const stations = JSON.parse(sessionStorageStations);
         return {
             routeName,
             routeId: Number(routeId),
-            stations: json.stations,
-            trains: json.trains
+            stations,
+            trains
         };
     })();
 
@@ -45,58 +47,81 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function setTrains() {
+        console.log(trains);
         document.querySelectorAll('thead > tr:last-child > th:nth-child(n + 2)').forEach(th => {
             th.remove();
         });
-        document.querySelector('thead > tr > th:last-child').setAttribute('colspan', trains.length);
-        trains.forEach(train => {
-            const id = train.id;
-            const th = document.createElement('th');
-            th.dataset.id = id;
-            th.textContent = train.number;
-            document.querySelector('thead > tr:last-child').appendChild(th);
-            document.querySelectorAll('tbody > tr').forEach(tr => {
-                const td = document.createElement('td');
-                td.dataset.id = id;
-                tr.appendChild(td);
+        let trainCount = 0;
+        Object.keys(trains).forEach(timetableNumber => {
+            trains[timetableNumber].trains.forEach(train => {
+                const id = train.id;
+                const th = document.createElement('th');
+                th.dataset.id = id;
+                th.textContent = train.number;
+                document.querySelector('thead > tr:last-child').appendChild(th);
+                document.querySelectorAll('tbody > tr').forEach(tr => {
+                    const td = document.createElement('td');
+                    td.dataset.id = id;
+                    tr.appendChild(td);
+                });
+                trainCount++;
             });
         });
-        trains.forEach(train => {
-            const trainId = train.id;
-            train.stations.forEach(station => {
-                const statinoId = station.id;
-                const arrTd = document.querySelector(`tr[data-id="${statinoId}"][data-des="arr"] > td[data-id="${trainId}"]`) ?? null;
-                if (arrTd !== null && station.arrival_time !== null) {
-                    arrTd.textContent = `${station.arrival_time}`.padStart(4, '0').match(/.{2}/g).join(':');
-                }
-                const depTd = document.querySelector(`tr[data-id="${statinoId}"][data-des="dep"] > td[data-id="${trainId}"]`) ?? null;
-                if (depTd !== null && station.departure_time !== null) {
-                    depTd.textContent = `${station.departure_time}`.padStart(4, '0').match(/.{2}/g).join(':');
-                }
+        document.querySelector('thead > tr > th:last-child').setAttribute('colspan', trainCount);
+        Object.keys(trains).forEach(timetableNumber => {
+            trains[timetableNumber].trains.forEach(train => {
+                const trainId = train.id;
+                const trainStationId = [];
+                train.stations.forEach(station => {
+                    const stationId = station.id;
+                    trainStationId.push(stationId);
+                    const arrTd = document.querySelector(`tr[data-id="${stationId}"][data-des="arr"] > td[data-id="${trainId}"]`) ?? null;
+                    if (arrTd !== null && station.arrival_time !== null) {
+                        arrTd.textContent = `${station.arrival_time}`.padStart(4, '0').match(/.{2}/g).join(':');
+                    }
+                    const depTd = document.querySelector(`tr[data-id="${stationId}"][data-des="dep"] > td[data-id="${trainId}"]`) ?? null;
+                    if (depTd !== null && station.departure_time !== null) {
+                        depTd.textContent = `${station.departure_time}`.padStart(4, '0').match(/.{2}/g).join(':');
+                    }
+                });
+                let isTrainStarted = false;
+                stations.forEach(station => {
+                    if (trainStationId.includes(station.id)) {
+                        isTrainStarted = true;
+                    }
+                    if (!isTrainStarted) {
+                        return;
+                    }
+                    const td = document.querySelector(`tr[data-id="${station.id}"][data-des="dep"] > td[data-id="${trainId}"]`);
+                    if (td.textContent.length === 0) {
+                        td.dataset.started = 'true';
+                    }
+                });
+                let isTrainEnded = true;
+                stations.reverse().forEach(station => {
+                    if (trainStationId.includes(station.id)) {
+                        isTrainEnded = false;
+                    }
+                    if (isTrainEnded) {
+                        return;
+                    }
+                    if (document.querySelector(`tr[data-id="${station.id}"][data-des="arr"] > td[data-id="${trainId}"]`).textContent.length !== 0) {
+                        return;
+                    }
+                    const td = document.querySelector(`tr[data-id="${station.id}"][data-des="dep"] > td[data-id="${trainId}"]`);
+                    if (td.textContent.length === 0) {
+                        td.dataset.ended = 'false';
+                    }
+                });
             });
         });
-        trains.forEach(train => {
-            const trainId = train.id;
-            const stops = [];
-            const tds = document.querySelectorAll(`tbody > tr > td[data-id="${trainId}"]`);
-            for (let i = 0; i < tds.length; i++) {
-                if (tds[i].textContent.length !== 0) {
-                    stops.push(i);
-                }
-            }
-            for (let i = 1; i < stops.length; i++) {
-                const lastStop = stops[i - 1];
-                const thisStop = stops[i];
-                for (let j = lastStop + 1; j < thisStop; j++) {
-                    const td = tds[j];
-                    td.dataset.passage = 'true';
-                    td.textContent = 'レ';
-                }
-            }
-            document.querySelectorAll('tbody > tr[data-des="arr"] > td[data-passage="true"]').forEach(td => {
-                td.setAttribute('rowspan', 2);
-            });
-            document.querySelectorAll('tbody > tr[data-des="dep"] > td[data-passage="true"]').forEach(td => {
+        document.querySelectorAll('tr[data-des="dep"]').forEach(tr => {
+            const stationId = tr.dataset.id;
+            tr.querySelectorAll('td[data-started="true"][data-ended="false"]').forEach(td => {
+                const trainId = td.dataset.id;
+                const arr = document.querySelector(`tr[data-id="${stationId}"][data-des="arr"] > td[data-id="${trainId}"]`);
+                arr.setAttribute('rowspan', 2);
+                arr.textContent = 'レ';
                 td.remove();
             });
         });
